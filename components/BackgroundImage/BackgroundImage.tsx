5fc5de80add98d613eda0f5a3e27e676022fbe7c
Brock Phillis <Brockphillis1992@gmail.com>
2023-02-08 21:31:46 +1000

    feat(scope): changes from earlier comments

--- components/BackgroundImage/BackgroundImage.tsx
+++ components/BackgroundImage/BackgroundImage.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React from "react";
 import { Image } from "@chakra-ui/react";
 
 type Props = {
@@ -7,12 +6,10 @@ type Props = {
 };
 
 export const BackgroundImage = ({ image }: Props) => {
-  const [bgImage, setBgImage] = useState(image);
-
   return (
     <>
       <Image
-        src={bgImage}
+        src={image}
         alt="loginBackground"
         fill="true"
         objectFit="cover"
